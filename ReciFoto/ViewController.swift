0c908c45e5370858e454cf3ee7a2937ae3147fa6
Colin Taylor <colintaylor@Colins-iMac.local>
2017-01-18 13:58:11 -0800

    Initial Commit

---
+++ ReciFoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReciFoto
+//
+//  Created by Colin Taylor on 1/18/17.
+//  Copyright © 2017 Colin Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
